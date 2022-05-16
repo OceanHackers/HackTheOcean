870e060827bc245325700e7bf6e95c4b25fe7ef0
Adrigondo <theadrigondo24@gmail.com>
2022-05-15 19:34:49 -0600

    Update: API V2

--- OceanHackers API/express_api/app.js
+++ OceanHackers API/express_api/app.js
@@ -1,93 +1,245 @@
 const express = require('express')
+const mysql = require('mysql')
+const fs = require('fs');
+
+const bodyParser = require('body-parser')
+
+const test_data =require('./test_data.json')
+
 const app = express()
 app.use(express.json())
-const port = 3000
-app.listen(port, () => {
- console.log(`Example app listening on port ${port}`)
+
+var connection=mysql.createConnection(
+    {
+        host:"oceanhackers.mysql.database.azure.com",
+        user:"oceanhackerAEADA", password:"Exception_Elongated_Native_Slashed_Barbell_Capsize",
+        database:"webpage_1",
+        port:3306,
+        ssl: {
+            ca:fs.readFileSync("./DigiCertGlobalRootCA.crt.pem")
+        }
+    }
+);
+
+connection.connect(error=>{
+    if(error) throw error;
+    console.log("Database server running!")
 })
 
-app.get('/v1/states', (req, res) => {
-    console.log(`Api States GET ALL request ${new Date()}`)
-    const state1 = {id: 1, name: "Baja California",
-    beach: "Playa Hermosa",
-    species: "Extensiones marinas, pantanos y turberas (superficies cubiertas de agua ya sea natural o artificial)",
-    problems: "Limite maximo de enterococos (microorganismos infecciosos producidos, entre otros por heces fecales)"}
+const PORT = process.env.PORT || 3000
 
-    const state2 = {id: 2, name: "Sonora",
-    beach: "San Carlos, Guaymas, Empalme",
-    species: "Ballena (Menor, Gris, Azul, Jorobada), Delfín, Orca, Caracol",
-    problems: "Presencia de enterococos y estreptococos por aguas negras"}
-
-    const state3 = {id: 3, name: "Sinaloa",
-    beach: "Las Glorias",
-    species: "Ballenas, Delfines, Lobos marinos, Foca, Morsa, Nutria, Manatíes",
-    problems: "Tiene 149 partículas de Enterococos feacalis por cada 100 mililitros de agua"}
-
-    const state4 = {id: 4, name: "Nayarit",
-    beach: "Sayulita",
-    species: "Tecolotes, Armadillos, Jabilies, Pumas, Jaguares, Pez dorado, Atun, Sierra, Marlin, Camaron, Pez vela",
-    problems: "Niveles superiores a los 200 enterococos (microorganismos infecciosos producidos, entre otros por heces fecales)"}
-    
-    const state5 = {id: 5, name: "Jalisco",
-    beach: "Los Muertos, Cuale",
-    species: "Ballenas, Delfines, Zifios",
-    problems: "Residuos orgánicos, Aguas residuales"}
-    
-    const state6 = {id: 6, name: "Colima",
-    beach: " Boca Pascuales, Pascuales, Tecomán, Playa Tecuanillo, El Real",
-    species: "Agujón, Dorado, Tiburón, Mantarraya, Delfín, Ballena jorobada, Lapa",
-    problems: "Residuos orgánicos"}
-    
-    const state7 = {id: 7, name: "Michoacan",
-    beach: "Maruata",
-    species: "Gaviotas, Garzas, Pelicanos, Cormoranes, Flamencos, Tortugas marinas, Golfina, Laud Negra",
-    problems: "Niveles de Dinophysis Caudata superiores permitidos, No aptos para el consumo de mariscos"}
-    
-    const state8 = {id: 8, name: "Guerrero",
-    beach: "Acapulco",
-    species: "Bosque de coniferas, Tecolotito, Jaguar, Ocelote, Oso hormiguero, Tigrillo",
-    problems: "Derrames de aguas negras, Residuos solidos urbanos"}
-    
-    const state9 = {id: 9, name: "Oaxaca",
-    beach: "Puerto Angelito",
-    species: "Tursiops truncatus, Stenella attenuata, Stenella longirostris, Feresa attenuata, Pseudorca crassidens, Grampus griseus, Globicephala macrorhynchus, Megaptera novaeangliae",
-    problems: "Residuos"}
-
-    const state10 = {id: 10, name: "Chiapas",
-    beach: "Puerto Mader",
-    species: "Mojarra del Petén, Cacomixtle, Ballena jorobada",
-    problems: "Residuos urbanos"}
-    
-    const state11 = {id: 11, name: "Quintana Roo",
-    beach: "Na",
-    species: "Cangrejo ermitaño, Coral, Camaroncillo, Langostino, Delfín",
-    problems: "Basura"}
-
-    const state12 = {id: 12, name: "Yucatan", 
-    area: "Golfo de Mexico",
-    species: "Tortugas marinas (4 en PE: Lora, Carey, Verde, Laud), Ballenas, Delfines, Arrecifes de coral, Humedales, Manglares",
-    problems: "Derrames de petroleo"}
-    
-    const state13 = {id: 13, name: "Campache",
-    area: "Golfo de Mexico",
-    species: "Tortugas marinas (4 en PE: Lora, Carey, Verde, Laud), Ballenas, Delfines, Arrecifes de coral, Humedales, Manglares",
-    problems: "Derrames de petroleo"}
-    
-    const state14 = {id: 14, name: "Tabasco",
-    area: "Golfo de Mexico",
-    species: "Tortugas marinas (4 en PE: Lora, Carey, Verde, Laud), Ballenas, Delfines, Arrecifes de coral, Humedales, Manglares",
-    problems: "Derrames de petroleo"}
-    
-    const state15 = {id: 15, name: "Veracruz",
-    area: "Golfo de Mexico",
-    species: "Tortugas marinas (4 en PE: Lora, Carey, Verde, Laud), Ballenas, Delfines, Arrecifes de coral, Humedales, Manglares",
-    problems: "Derrames de petroleo"}
-    
-    const state16 = {id: 16, name: "Tamaulipas",
-    area: "Golfo de Mexico",
-    species: "Tortugas marinas (4 en PE: Lora, Carey, Verde, Laud), Ballenas, Delfines, Arrecifes de coral, Humedales, Manglares",
-    problems: "Derrames de petroleo"}
-    
-    const states = [state1, state2, state3, state4, state5, state6, state7, state8, state9, state10, state11, state12, state13, state14, state15, state16]
-    res.status(200).json(states)
+app.listen(PORT, () => {
+ console.log(`Server listening on PORT ${PORT}`)
 })
+
+// Test API is working
+// app.get('/', (req, res) => {
+//     console.log(`Api States GET ALL request ${new Date()}`)
+//     const states=[];
+//     test_data.missions.forEach(mission => {
+//         states.push(mission)
+//     });
+//     console.log(states);
+//     res.status(200).json(states)
+// })
+
+// CALL TO ADD TEST DATA 
+app.post("/test", (req, res)=>{
+    const images=test_data.images;
+    const missions=test_data.missions;
+    const challenges=test_data.challenges;
+    const challenges_images=test_data.challenges_images;
+    const users=test_data.users;
+    const resources=test_data.resources
+
+    var it=0
+    var object={}
+    images.forEach(image => {
+        const sql=`INSERT INTO images SET ?`
+        console.log(image)
+        connection.query(sql, image, error=>{
+            if(error) throw error;
+            console.log(it+" - upload image complete!")
+            it++
+        })
+    });
+    const sql=`SELECT * FROM images`
+    connection.query(sql,(error, results)=>{
+        if(error) throw error;
+        if(results.length>0){
+            res.status(200).json(results)
+        }else{
+            res.status(400).json(null);
+        }
+    })
+
+    it=0
+    missions.forEach(mission => {
+        const sql=`INSERT INTO missions SET ?`
+        console.log(mission)
+        connection.query(sql, mission, error=>{
+            if(error) throw error;
+            console.log(it+" - upload mission complete!")
+        })
+        it++
+    });
+
+
+    it=0
+    challenges.forEach(challenge => {
+        const sql=`INSERT INTO challenges SET ?`
+        console.log(challenge)
+        connection.query(sql, challenge, error=>{
+            if(error) throw error;
+            console.log(it+" - upload challenge complete!")
+        })
+        it++
+    });
+
+    it=0
+    challenges_images.forEach(challenge_image => {
+        const sql=`INSERT INTO challenges_images SET ?`
+        console.log(challenge_image)
+        connection.query(sql, challenge_image, error=>{
+            if(error) throw error;
+            console.log(it+" - upload challenge_image complete!")
+        })
+        it++
+    });
+    
+    it=0
+    users.forEach(user => {
+        const sql=`INSERT INTO users SET ?`
+        console.log(user)
+        connection.query(sql, user, error=>{
+            if(error) throw error;
+            console.log(it+" - upload user complete!")
+        })
+        it++
+    });
+    
+    it=0
+    resources.forEach(resource => {
+        const sql=`INSERT INTO resources SET ?`
+        console.log(resource)
+        connection.query(sql, resource, error=>{
+            if(error) throw error;
+            console.log(it+" - upload resource complete!")
+        })
+        it++
+    });
+
+})
+
+app.get("/missions", (req,res)=>{
+    const sql="SELECT * FROM missions"
+    connection.query(sql,(error, results)=>{
+        if(error) throw error;
+        if(results.length>0){
+            res.status(200).json(results)
+        }else{
+            res.status(400).json(null);
+        }
+    })
+})
+
+app.get("/users", (req,res)=>{
+    const sql="SELECT * FROM users"
+    connection.query(sql,(error, results)=>{
+        if(error) throw error;
+        if(results.length>0){
+            res.status(200).json(results)
+        }else{
+            res.status(400).json(null);
+        }
+    })
+})
+app.get("/missions/:id", (req,res)=>{
+    const {id} = req.params;
+    const sql=`SELECT * FROM missions WHERE id_mission = ${id} `
+    connection.query(sql,(error, result)=>{
+        if(error) throw error;
+        if(result.length>0){
+            res.status(200).json(result[0])
+        }else{
+            res.status(400).json(null);
+        }
+    })
+})
+
+app.get("/challenges/:id", (req,res)=>{
+    const {id} = req.params;
+    const sql=`SELECT * FROM challenges WHERE id_challenge = ${id} `
+    connection.query(sql,(error, result)=>{
+        if(error) throw error;
+        if(result.length>0){
+            // sql=`SELECT * FROM images WHERE id = 1 `
+            res.status(200).json(result[0])
+        }else{
+            res.status(400).json(null);
+        }
+    })
+})
+app.get("/resources/:id", (req,res)=>{
+    const {id} = req.params;
+    const sql=`SELECT * FROM resources WHERE id_resource = ${id} `
+    connection.query(sql,(error, result)=>{
+        if(error) throw error;
+        if(result.length>0){
+            res.status(200).json(result[0])
+        }else{
+            res.status(400).json(null);
+        }
+    })
+})
+app.post("/login/", (req,res)=>{
+    // console.log(req.body)
+    var object_result;
+    var email = req.body.email;
+    email=email.replace("@","")
+    var password = req.body.password;
+    const sql=`SELECT * FROM users HAVING email = '${email}' `
+    connection.query(sql,(error, result)=>{
+        if(error) throw error;
+        if(result.length>0){
+            object_result = Object.values(JSON.parse(JSON.stringify(result)));
+            // console.log(object_result[0].PASSWORD, password)
+            if(object_result[0].PASSWORD!=password){
+                res.status(400).json(false);
+            }else{
+                console.log(true)
+                res.status(200).json(true)
+            }
+        }else{
+            res.status(400).json(null);
+        }
+    })
+})
+/* 
+app.post("/missions/add", (req,res)=>{
+    const {id} = req.params;
+    
+    const sql=`INSERT INTO missions SET ?`
+    const tag={
+        name: req.body.name,
+        value: req.body.value,
+    }
+    const imgObject={
+        url: req.body.url,
+        description: req.body.description,
+    }
+    const missionObject={
+        coord_x: req.body.coord_x,
+        coord_y: req.body.coord_y,
+        tilte: req.body.title,
+    }
+
+    connection.query(sql,(error, result)=>{
+        if(error) throw error;
+        if(result.length>0){
+            res.status(200).json(result)
+        }else{
+            res.status(400).json(null);
+        }
+    })
+}) */
